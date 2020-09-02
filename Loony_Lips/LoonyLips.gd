4629956965d9962f10dd095b59453640729dd170
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-09-01 23:22:50 -0300

    Exemplos de possiveis histórias

--- Loony_Lips/LoonyLips.gd
+++ Loony_Lips/LoonyLips.gd
@@ -1,4 +1,14 @@
 extends Control
 
 func _ready():
-	print("Era uma vez, Daiane assistiu Minions e pensou isse foi o melhor filme das ultimas duas décadas.")
+	# var person = "Daiane"
+	var prompts = ["Daiane", "banana", "feliz", "glorioso"]
+	#var story = "Era uma vez, %s assistiu %s e pensou esse foi o %s filme das ultimas duas décadas."
+	var story = "Era uma vez, alguém chamado %s comeu um sanduiche com gosto de %s que fez se sentir %s, por dentro. Este foi um dia %s."
+	print(story % prompts)
+	
+	prompts = ["Snow", "atum", "agitado", "engraçado"]
+	print(story % prompts)
+	
+	
+	
