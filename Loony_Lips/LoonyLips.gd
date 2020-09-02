91b455c69e89cda6bc3cee7a2f197be1a106f0d6
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-09-02 18:51:12 -0300

    Exibindo história na Label

--- Loony_Lips/LoonyLips.gd
+++ Loony_Lips/LoonyLips.gd
@@ -2,13 +2,12 @@ extends Control
 
 func _ready():
 	# var person = "Daiane"
-	var prompts = ["Daiane", "banana", "feliz", "glorioso"]
+	var prompts = ["Daiane", "acai", "explendida", "fantastico"]
 	#var story = "Era uma vez, %s assistiu %s e pensou esse foi o %s filme das ultimas duas décadas."
 	var story = "Era uma vez, alguém chamado %s comeu um sanduiche com gosto de %s que fez se sentir %s, por dentro. Este foi um dia %s."
 	print(story % prompts)
 	
-	prompts = ["Snow", "atum", "agitado", "engraçado"]
-	print(story % prompts)
+	$DisplayText.text = story % prompts
 	
 	
 	
