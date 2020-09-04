d5b2adac3166fffb8f6b3f01f8b47342f2a76254
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-09-03 23:26:23 -0300

    Adicionando campo de texto e organizando componentes nas Scene

--- Loony_Lips/LoonyLips.gd
+++ Loony_Lips/LoonyLips.gd
@@ -5,9 +5,8 @@ func _ready():
 	var prompts = ["Daiane", "acai", "explendida", "fantastico"]
 	#var story = "Era uma vez, %s assistiu %s e pensou esse foi o %s filme das ultimas duas décadas."
 	var story = "Era uma vez, alguém chamado %s comeu um sanduiche com gosto de %s que fez se sentir %s, por dentro. Este foi um dia %s."
-	print(story % prompts)
 	
-	$DisplayText.text = story % prompts
+	$VBoxContainer/DisplayText.text = story % prompts
 	
 	
 	
