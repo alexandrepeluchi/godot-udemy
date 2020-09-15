d2b828bc3e59fe193b31fcb80af5ad308d50a04f
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-09-15 20:25:14 -0300

    Gravidade e ação de pular

--- Hoppy Days/Player/Player.gd
+++ Hoppy Days/Player/Player.gd
@@ -1,9 +1,19 @@
 extends KinematicBody2D
 
 var motion = Vector2(0, 0)
-const SPEED = 500
+
+const SPEED = 1000
+const GRAVITY = 100
+const UP = Vector2(0 ,-1)
+const JUMP_SPEED = 2000
 
 func _physics_process(delta):
+	apply_gravity()
+	move()
+	jump()
+	move_and_slide(motion, UP)
+	
+func move():
 	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
 		motion.x = -SPEED
 	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
@@ -11,4 +21,16 @@ func _physics_process(delta):
 	else:
 		motion.x = 0
 
-	move_and_slide(motion)
+func jump():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		motion.y -= JUMP_SPEED
+
+func apply_gravity():
+	if is_on_floor():
+		motion.y = 0
+	else:
+		motion.y += GRAVITY
+		
+
+
+
