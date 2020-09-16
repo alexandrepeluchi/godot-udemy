9e16c0f933d10a93e25b93c10c2c460971b44d7f
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-09-16 18:40:48 -0300

    Refatorando o código utilizando o Principio da Responsabilidade Única

--- Hoppy Days/Player/Player.gd
+++ Hoppy Days/Player/Player.gd
@@ -7,6 +7,8 @@ const GRAVITY = 100
 const UP = Vector2(0 ,-1)
 const JUMP_SPEED = 2000
 
+signal animate
+
 func _physics_process(delta):
 	apply_gravity()
 	move()
@@ -33,16 +35,7 @@ func apply_gravity():
 		motion.y += GRAVITY
 		
 func animate():
-	if motion.y < 0:
-		$AnimatedSprite.play("jump")
-	elif motion.x > 0:
-			$AnimatedSprite.play("walk")
-			$AnimatedSprite.flip_h = false
-	elif motion.x < 0:
-			$AnimatedSprite.play("walk")
-			$AnimatedSprite.flip_h = true	
-	else:
-		$AnimatedSprite.play("idle")
+	emit_signal("animate", motion)
 
 
 
