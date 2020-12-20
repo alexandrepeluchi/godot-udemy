2a640ddf40629d2d80e810447be32fe0e5c47497
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-12-20 17:16:04 -0300

    Moedas aumentam a vida do jogador

--- Hoppy Days/Levels/GameState.gd
+++ Hoppy Days/Levels/GameState.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 export var lives = 3
 export var coins = 0
+export var target_number_of_coins = 10
 
 func _ready():
 	add_to_group("Gamestate")
@@ -16,11 +17,21 @@ func hurt():
 		end_game()
 
 func update_GUI():
-	get_tree().call_group("GUI", "update_lives", lives)	
+	get_tree().call_group("GUI", "update_GUI", lives, coins)	
 
 func coin_up():
 	coins += 1
-	get_tree().call_group("GUI", "update_coins", coins)		
+	update_GUI()	
+	get_tree().call_group("GUI", "update_coins", coins)
+	var multiple_of_coins = (coins % target_number_of_coins) == 0
+	
+	if multiple_of_coins:
+		life_up()
+		coins = 0		
+
+func life_up():
+	lives += 1
+	update_GUI()
 
 func end_game():
 	get_tree().change_scene("res://Levels/GameOver.tscn")
