659ce6082b448e364a037f5f3f2929c3d53c0749
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-12-21 21:37:06 -0300

    Corrigido o glitch de não funcionar a o jumpad e o spike

--- Hoppy Days/Player/Player.gd
+++ Hoppy Days/Player/Player.gd
@@ -34,7 +34,7 @@ func jump():
 func apply_gravity():
 	if position.y > WORLD_LIMIT:
 		get_tree().call_group("Gamestate", "end_game")
-	if is_on_floor():
+	if is_on_floor() and motion.y > 0:
 		motion.y = 0
 	elif is_on_ceiling():
 		motion.y = 1
@@ -47,7 +47,7 @@ func animate():
 func hurt():
 	position.y -= 1
 	yield(get_tree(), "idle_frame")
-	motion.y -= JUMP_SPEED
+	motion.y = -JUMP_SPEED
 	$PainSFX.play()	
 #	if lives < 0:
 #		end_game()
@@ -55,4 +55,4 @@ func hurt():
 func boost():
 	position.y -= 1
 	yield(get_tree(), "idle_frame")
-	motion.y -= JUMP_SPEED * BOOST_MULTIPLIER
+	motion.y = -JUMP_SPEED * BOOST_MULTIPLIER
