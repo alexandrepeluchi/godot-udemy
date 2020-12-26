6de2384e0bc0a5600801c7fbe1eab9a657af1857
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-12-26 18:16:29 -0300

    Correção nos controles e animação para quando Player é machucado

--- Hoppy Days/Player/PlayerAnimation.gd
+++ Hoppy Days/Player/PlayerAnimation.gd
@@ -1,8 +1,10 @@
 extends AnimatedSprite
 
 func _on_Player_animate(motion):
-	if motion.y < 0:
+	if motion.y < 0 and Input.is_action_pressed("jump"):
 		play("jump")
+	elif motion.y < 0 and not Input.is_action_pressed("jump"):
+		play("hurt")
 	elif motion.x > 0:
 		play("walk")
 		flip_h = false
