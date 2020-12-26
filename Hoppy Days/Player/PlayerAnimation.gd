515fd4107f93b1ccd6837b24345c256a8973dff2
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-12-26 19:00:16 -0300

    Removido animação de machucado

--- Hoppy Days/Player/PlayerAnimation.gd
+++ Hoppy Days/Player/PlayerAnimation.gd
@@ -1,10 +1,8 @@
 extends AnimatedSprite
 
 func _on_Player_animate(motion):
-	if motion.y < 0 and Input.is_action_pressed("jump"):
+	if motion.y < 0:
 		play("jump")
-	elif motion.y < 0 and not Input.is_action_pressed("jump"):
-		play("hurt")
 	elif motion.x > 0:
 		play("walk")
 		flip_h = false
