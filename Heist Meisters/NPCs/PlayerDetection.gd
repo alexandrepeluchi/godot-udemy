4ecef26fb424abd702ab525c816bd1e011fe3fb4
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2021-01-07 20:19:13 -0300

    Reduzida a distância da visão de detecção do jogador

--- Heist Meisters/NPCs/PlayerDetection.gd
+++ Heist Meisters/NPCs/PlayerDetection.gd
@@ -1,6 +1,7 @@
 extends "res://Characters/TemplateCharacter.gd"
 
 const FOV_TOLERANCE: float = 20.0
+const MAX_DETECTION_RANGE: int = 640
 const RED := Color(1, 0.25, 0.25)
 const WHITE := Color(1, 1, 1)
 
@@ -32,8 +33,9 @@ func Player_in_LOS() -> bool:
 		return false
 	
 	var distance_to_player = Player.global_position.distance_to(global_position)
+	var Player_in_Range = distance_to_player < MAX_DETECTION_RANGE	
 	
-	if LOS_obstacle.collider == Player:
+	if LOS_obstacle.collider == Player and Player_in_Range:
 		return true
 	else:
 		return false
