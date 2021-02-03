4712c1a10760a07fe8191b56e14e51e2beaf27ad
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2021-02-02 22:43:33 -0300

    Conectando sinais e criação da tela do computador

--- Heist Meisters/Computer/Computer.gd
+++ Heist Meisters/Computer/Computer.gd
@@ -1,2 +1,14 @@
 extends Node2D
 
+
+
+func _on_Area2D_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_Area2D_body_exited(body):
+	pass # Replace with function body.
+
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	pass # Replace with function body.
