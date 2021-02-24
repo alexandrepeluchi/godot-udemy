f1987ce9b7f2b3c623e6d411b678691fdcb2ba93
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2021-02-23 22:31:33 -0300

    Diminuindo a velocidade do jogador ao se movimentar disfarçado e um timer de duração

--- Heist Meisters/Characters/Player.gd
+++ Heist Meisters/Characters/Player.gd
@@ -10,11 +10,19 @@ const BOX_SPRITE = "res://GFX/PNG/Tiles/tile_130.png"
 const BOX_LIGHT = "res://GFX/PNG/Tiles/tile_130.png"
 const BOX_OCCLUDER = "res://Characters/BoxOccluder.tres"
 
+var velocity_multiplier: float = 1.0
+
 var disguised: bool = false
+export var disguise_slowdown: float = 0.25
+export var disguise_duration: float = 5.0
+
+func _ready():
+	$Timer.wait_time = disguise_duration
+	reveal()
 
 func _physics_process(delta: float) -> void:
 	update_movement()
-	move_and_slide(motion)
+	move_and_slide(motion * velocity_multiplier)
 	
 func update_movement() -> void:
 	look_at(get_global_mouse_position())
@@ -48,6 +56,9 @@ func reveal():
 	$Sprite.texture = load(PLAYER_SPRITE)
 	$Light2D.texture = load(PLAYER_LIGHT)	
 	$LightOccluder2D.occluder = load(PLAYER_OCCLUDER)
+	
+	velocity_multiplier = 1.0
+	
 	disguised = false
 	collision_layer = 1
 	
@@ -55,6 +66,10 @@ func disguise():
 	$Sprite.texture = load(BOX_SPRITE)
 	$Light2D.texture = load(BOX_LIGHT)
 	$LightOccluder2D.occluder = load(BOX_OCCLUDER)
+	
+	velocity_multiplier = disguise_slowdown
+	
 	disguised = true
 	collision_layer = 16
+	$Timer.start()
 	
