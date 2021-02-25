8a5f99f2bb86104f76dae4006962f6393d0d85fc
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2021-02-25 19:25:42 -0300

    Exibe na HUD a quantidade de disfarces restantes

--- Heist Meisters/Characters/Player.gd
+++ Heist Meisters/Characters/Player.gd
@@ -19,6 +19,7 @@ export var number_of_disguises: int = 3
 
 func _ready():
 	$Timer.wait_time = disguise_duration
+	get_tree().call_group("DisguiseDisplay", "update_disguises", number_of_disguises)
 	reveal()
 
 func _physics_process(delta: float) -> void:
@@ -76,6 +77,7 @@ func disguise():
 	
 	velocity_multiplier = disguise_slowdown
 	number_of_disguises -= 1
+	get_tree().call_group("DisguiseDisplay", "update_disguises", number_of_disguises)	
 	
 	disguised = true
 	collision_layer = 16
---
+++ Heist Meisters/Interface/DisguiseCounter.gd
@@ -0,0 +1,7 @@
+extends ItemList
+
+func update_disguises(number):
+	clear()
+	
+	for disguises in range(number):
+		add_icon_item(load("res://GFX/PNG/Tiles/tile_129.png"), false)
